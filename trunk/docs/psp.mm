f68a701d4d35e32bf1136588b0cb9e8af463de14
danpo <danpo@cef5e46f-a703-0410-a4eb-a1b77d141377>
2005-12-09 17:41:02 +0000

    new psp and other improvements

---
+++ trunk/docs/psp.mm
@@ -0,0 +1,51 @@
+<map version="0.7.1">
+<node TEXT="Brainfuck Studio">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="BIDL" POSITION="right">
+<node TEXT="processing of the intermediate language"/>
+<node TEXT="debugging "/>
+</node>
+<node TEXT="Interpreter" POSITION="right">
+<node TEXT="command-line frontend of the BIDL"/>
+<node TEXT="step-by-step execution"/>
+</node>
+<node TEXT="Compiler" POSITION="right">
+<node TEXT="code generation">
+<node TEXT="Windows (R)"/>
+<node TEXT="Unix"/>
+<node TEXT="Mac OS X"/>
+</node>
+<node TEXT="command-line frontend of the BPL">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node TEXT="Documentation" POSITION="left">
+<node TEXT="user manual"/>
+<node TEXT="API documentation"/>
+</node>
+<node TEXT="GUI" POSITION="left">
+<node TEXT="basic functionality">
+<node TEXT="copy &amp; paste"/>
+<node TEXT="undo etc."/>
+<node TEXT="shortcuts"/>
+<node TEXT="search &amp; replace"/>
+<node TEXT="editing of multiple files"/>
+</node>
+<node TEXT="syntax highlighting"/>
+<node TEXT="debugging">
+<node TEXT="using the BIDL"/>
+<node TEXT="output of the results"/>
+</node>
+<node TEXT="integration of the development tools"/>
+</node>
+<node TEXT="BPL" POSITION="right">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node TEXT="error detection"/>
+<node TEXT="optimization"/>
+<node TEXT="generation of the intermediate language"/>
+</node>
+</node>
+</map>
